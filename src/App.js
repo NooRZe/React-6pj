5adb73a7eee6b9bb1f4625346aa62b0dce3b70bf
NooRZe <109274539+NooRZe@users.noreply.github.com>
2025-04-22 11:27:30 +0300

    Update App.js исправил индекс правильного ответа

--- src/App.js
+++ src/App.js
@@ -5,47 +5,47 @@ const questions = [
   {
    title: 'Сколько всего играбельных фракций насчитывает трилогия Dawn of War?',
    variants: ['3', '4', '7', '10'],
-   correct: 4, 
+   correct: 3, 
   },
   {
     title: 'Сколько существовало легионов первого основания?',
     variants: ['14', '20', '31', '23'],
-    correct: 2, 
+    correct: 1, 
    },
    {
     title: 'Какой мод для Soulstorm считается самым масштабным по состоянию на 2025 год?',
     variants: ['Ultimate Apocalypse', 'Chaos Daemons', 'Eternal Confrontation', 'Tyranid mod'],
-    correct: 1, 
+    correct: 0, 
    },
    {
     title: 'Кто является создателем Кодекса Астартес?',
     variants: ['Император человечества', 'Солар Махарий', 'Хорус Луперкаль', 'Робаут Жиллиман'],
-    correct: 4, 
+    correct: 3, 
    },
    {
     title: 'Какая раса присутствует не по всех играх Dawn of War?',
     variants: ['Эльдары', 'Некроны', 'Хаос', 'Космодесант'],
-    correct: 2, 
+    correct: 1, 
    },
    {
     title: 'Какой бог хаоса является Владыкой Перемен?',
     variants: ['Слаанеш', 'Тзинч', 'Кхорн', 'Нургл'],
-    correct: 2, 
+    correct: 1, 
    },
    {
     title: 'У какой расы в Soulstorm можно призвать одновременно наибольшее количество эпических юнитов?',
     variants: ['Хаос', 'Некроны', 'Сестры битвы', 'Империя Тау'],
-    correct: 1, 
+    correct: 0, 
    },
    {
     title: 'Кто из нижеперечисленных является героем Второй Войны за Армагеддон?',
     variants: ['Магистр Данте', 'Бьорн Разящая Рука', 'Комиссар Ярик', 'Хорус Луперкаль'],
-    correct: 3, 
+    correct: 2, 
    },
    {
     title: 'Какая раса является главным противником в Dawn of War 2?',
     variants: ['Хаос', 'Орки', 'Эльдары', 'Тираниды'],
-    correct: 4, 
+    correct: 3, 
    },
   
 ]
@@ -89,7 +89,7 @@ function App() {
   const onClickVAriant = (index) => {
     console.log(step, index);
     setStep(step +1);
-    if(index === (question.correct - 1)) {
+    if(index === (question.correct)) {
       setCorrect(correct + 1);
     }
   };
